893cb35852d5c044dd05850cb425aab1c0446064
semaerensoyy <s.erensoy.22@unimail.winchester.ac.uk>
2025-03-27 15:30:09 +0000

    fixed

---
+++ frontend/build/precache-manifest.36cfc79af3144eaefb1c3613e2c15092.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecaf4c07b72ace09019452ac4c9bf75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "159a28310c8fa83eff43",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "a58f409fc86b85972426",
+    "url": "/static/css/main.fe55ed1f.chunk.css"
+  },
+  {
+    "revision": "159a28310c8fa83eff43",
+    "url": "/static/js/2.716e5e4f.chunk.js"
+  },
+  {
+    "revision": "72daec69efe3b91d5b55b21df81015f7",
+    "url": "/static/js/2.716e5e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58f409fc86b85972426",
+    "url": "/static/js/main.16f814f1.chunk.js"
+  },
+  {
+    "revision": "7bb91c2cd1f8f2f009f5",
+    "url": "/static/js/runtime-main.6cfffd99.js"
+  }
+]);
