e3ce70de5fee3c1952891cf0488545a5c49327e3
semaerensoyy <s.erensoy.22@unimail.winchester.ac.uk>
2025-03-27 14:18:44 +0000

    fixed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "firewardentracker-apggb8hzfkfsbjf3.uksouth-01.azurewebsites.net", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
