4f7dc7a533e0f3591e2fa72f0f24ec5f50cc6451
semaerensoyy <s.erensoy.22@unimail.winchester.ac.uk>
2025-03-27 14:03:08 +0000

    fixed

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import React from 'react';
 
 test('renders Fire Warden Tracker header', () => {
   render(
